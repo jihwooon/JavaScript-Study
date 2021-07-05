1d786711b4d8502a89a4fc05e1d44012fb0e4e21
안지환 <>
2021-07-06 00:18:15 +0900

    2021.07.06

---
+++ ES2021/app.js
@@ -0,0 +1,13 @@
+var a = 'abc';
+console.log(a);
+
+a = a + 'def';
+
+console.log(a);
+
+
+var b;
+b = 'abc';
+
+
+
---
+++ Node_DOCKER/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("<h2>Hello There</h2>");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));

8769995a630e5af3acdb6c9f8696bbad39981bad
안지환 <>
2021-07-11 00:57:20 +0900

    2021.07.11 test

--- node-rest-toutorial/ormconfig.js
+++ node-rest-toutorial/ormconfig.js
@@ -4,9 +4,9 @@ module.exports = {
     "type": "mysql",
     "host": "localhost",
     "port": 3306,
-    "username": "eastflag",
+    "username": "jihwooon",
     "password": "12345678",
-    "database": "eastdb",
+    "database": "node",
     "synchronize": true,
     "logging": false,
     "entities": [

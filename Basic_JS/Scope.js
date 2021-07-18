a7ad6c3e5e86874f9230a1a10349fe271d6cd90d
안지환 <>
2021-07-18 23:28:01 +0900

    JS_09 2021.07.18

--- Basic_JS/Scope.js
+++ Basic_JS/Scope.js
@@ -1,24 +1,38 @@
-function add(x, y) {
-  //매개변수는 함수 몸체 내부에서만 참조할 수 있다.
-  //즉, 매개변수의 스코프(유효범위)는 함수 몸체 내부다.
-  console.log(x, y); 
-  return x + y; //리턴 못 받음
-}
-add(2,5);
+// function add(x, y) {
+//   //매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+//   //즉, 매개변수의 스코프(유효범위)는 함수 몸체 내부다.
+//   console.log(x, y);
+//   return x + y; //리턴 못 받음
+// }
+// add(2,5);
+//
+// //매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+// // console.log(x,y); //ReferenceError: x is not defined
+//
+// function test(y,z) {
+//
+//   console.log(y,z);
+// }
+//
+// test(5,6);
+//
+//
+// var count = 0;
+// var timer = setInterval(function () {
+// 	console.log(count);
+// 	if(++count > 4) clearInterval(timer);
+// }, 300);
+//
 
-//매개변수는 함수 몸체 내부에서만 참조할 수 있다.
-// console.log(x,y); //ReferenceError: x is not defined
+var a = 1;
+var outer = function () {
+    var inner = function () {
+        var a = 3;
+        console.log(a);
 
-function test(y,z) {
-  
-  console.log(y,z);
-}
-
-test(5,6);
-
-
-var count = 0;
-var timer = setInterval(function () {
-	console.log(count);
-	if(++count > 4) clearInterval(timer);
-}, 300);
+    };
+    inner();
+    console.log(a);
+};
+outer();
+console.log(a);

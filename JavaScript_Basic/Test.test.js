a542ffd2d741f506a6f36eed841a735b8320b81c
jihwooon <jihwooon@gmail.com>
2022-09-10 00:37:25 +0900

    Test

---
+++ JavaScript_Basic/Test.test.js
@@ -0,0 +1,35 @@
+// const color1 = "red";
+// let color2 = "blue";
+// var color3 = "green";
+//
+// if(true) {
+//   const color1 = "yellow";
+//   let color2 = "yellow";
+//   var color3 = "yellow";
+//   console.log(color1);
+//   console.log(color2);
+//   console.log(color3);
+// }
+// console.log(color1);
+// console.log(color2);
+// console.log(color3);
+//
+//
+// function Contain(arr) {
+//   var container = Array(arr);
+//   for(var i = 0; i<5; i++) {
+//     container.push(function () {
+//       console.log(`나는 ${i}번 입니다.`);
+//     });
+//   }
+// }
+
+
+var i;
+for (i = 0; i < 10; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
+
+
